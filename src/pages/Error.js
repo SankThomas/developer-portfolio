bb9d855a4a72ce06cb2a8c555ce503118cc931fe
Thomas Sankara <thomassankara74@gmail.com>
2021-05-06 16:59:41 +0300

    style Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,23 @@
 const Error = () => {
   return (
     <>
-      <h1>Error 404. Page Not Found</h1>
+      <section
+        className="error"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h1>Error 404. Page Not Found</h1>
+        <Link
+          to="/"
+          style={{ backgroundColor: "#00b392", padding: 10, color: "white" }}
+        >
+          Homepage
+        </Link>
+      </section>
     </>
   )
 }
